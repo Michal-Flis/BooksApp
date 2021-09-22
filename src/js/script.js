208930c2c52795989ca205ab69e56809bc5c6009
Michał Flis <michal.flis@op.pl>
2021-09-22 17:01:13 +0200

    add function initAction

--- src/js/script.js
+++ src/js/script.js
@@ -8,22 +8,46 @@
     containerOf: {
       bookList: '.book-list'
     },
-    booksImages: '.book-image .book-list'
+    booksImages: {
+      images: '.book-image .book-list'
+    },
   };
 
   const templates = {
     bookTemplate: Handlebars.compile(document.querySelector(select.templateOf.bookTemplate).innerHTML),
   };
 
-
-  function rander() {
-    const thisBook = this;
+  // Funkcje
+  function render() {
+    // const thisBook = this;
 
     for (let allBooks of dataSource.books) {
       const generatedHTML = templates.bookTemplate(allBooks);
-      thisBook.element = utils.createDOMFromHTML(generatedHTML);
+      const element = utils.createDOMFromHTML(generatedHTML);
       const bookListContainer = document.querySelector(select.containerOf.bookList);
-      bookListContainer.appendChild(thisBook.element);
+      bookListContainer.appendChild(element);
     }
   }
+
+
+  function initAction() {
+    // const thisBook = this;
+
+    const favoriteBooks = [];
+
+    const bookImage = document.querySelector(select.booksImages.images);
+
+    for (let image of bookImage) {
+      image.addEventListener('dblclick', function (event) {
+        event.preventDefault();
+        image.classList.add('favorite');
+        const bookId = image.getAttribute('data-id');
+        favoriteBooks.push(bookId);
+      });
+    }
+  }
+
+  //   Wywoływanie funkcji
+  render();
+  initAction();
 }
