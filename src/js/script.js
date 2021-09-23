e68b8e2b80a0e0e4d86cf17e05591cf05037bad5
Michał Flis <michal.flis@op.pl>
2021-09-24 00:07:43 +0200

    bookList and form stage one

--- src/js/script.js
+++ src/js/script.js
@@ -6,16 +6,24 @@
       bookTemplate: '#template-book'
     },
     containerOf: {
-      bookList: '.book-list'
+      bookList: '.books-list',
+      imageClassName: 'book__image',
     },
-    booksImages: {
-      images: '.book-image .book-list'
+    details: {
+      form: '.filters',
     },
   };
 
   const templates = {
     bookTemplate: Handlebars.compile(document.querySelector(select.templateOf.bookTemplate).innerHTML),
   };
+  const favoriteBooks = [];
+
+  const filters = [];
+
+  const bookList = document.querySelector(select.containerOf.bookList);
+
+  const forms = document.querySelector(select.details.form);
 
   // Funkcje
   function render() {
@@ -23,8 +31,11 @@
 
     for (let allBooks of dataSource.books) {
       const generatedHTML = templates.bookTemplate(allBooks);
+
       const element = utils.createDOMFromHTML(generatedHTML);
+
       const bookListContainer = document.querySelector(select.containerOf.bookList);
+
       bookListContainer.appendChild(element);
     }
   }
@@ -33,18 +44,51 @@
   function initAction() {
     // const thisBook = this;
 
-    const favoriteBooks = [];
+    bookList.addEventListener('dblclick', function (event) {
 
-    const bookImage = document.querySelector(select.booksImages.images);
+      event.preventDefault();
+      // image.classList.add('favorite');
+      const book = event.target.offsetParent;
 
-    for (let image of bookImage) {
-      image.addEventListener('dblclick', function (event) {
-        event.preventDefault();
-        image.classList.add('favorite');
-        const bookId = image.getAttribute('data-id');
-        favoriteBooks.push(bookId);
-      });
-    }
+      if (book.classList.contains(select.containerOf.imageClassName)) {
+
+        const id = book.getAttribute('data-id');
+        // favoriteBooks.push(bookId);
+        if (!favoriteBooks.includes(id)) {
+
+          book.classList.add('favorite');
+
+          favoriteBooks.push(id);
+
+        } else {
+
+          favoriteBooks.splice(favoriteBooks.indexOf(id), 1);
+
+          book.classList.remove('favorite');
+        }
+      }
+    });
+    // console.log('abcd');
+    forms.addEventListener('change', function (event) {
+        // console.log('aaa');
+      event.preventDefault();
+        // console.log('bbb');
+      const formElem = event.target;
+        // console.log(formElem);
+      if (formElem.type === 'checkbox' && formElem.name === 'filter') {
+        if(formElem.checked){
+            filters.push(formElem.value);
+            
+        }else{
+            filters.splice(filters.indexOf(formElem.value), 1);
+            
+        }
+        }
+        console.log(filters);
+
+
+
+    });
   }
 
   //   Wywoływanie funkcji
